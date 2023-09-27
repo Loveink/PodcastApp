f5e9479d832e1006047e080b1da618cd3ff0469b
Caroline Tikhomirova <sonyukcv@gmail.com>
2023-09-27 23:28:28 +0400

    add some UIs

--- PodcastApp/Extensions/CustomColors.swift
+++ PodcastApp/Extensions/CustomColors.swift
@@ -8,24 +8,40 @@
 import UIKit
 
 extension UIColor {
-
-  static var textGrey: UIColor {
-    return UIColor(red: 0.522, green: 0.514, blue: 0.569, alpha: 1)
-  }
-
-  static var blueMiniPlayer: UIColor {
-    return UIColor(red: 0.8, green: 0.882, blue: 0.984, alpha: 1)
-  }
-
-  static var bluePlayer: UIColor {
-    return UIColor(red: 0.157, green: 0.51, blue: 0.945, alpha: 1)
-  }
-
-  static var blueBorder: UIColor {
-    return UIColor(red: 0.157, green: 0.51, blue: 0.945, alpha: 1)
-  }
-
-  static var deleteColor: UIColor {
-    return UIColor(red: 0.898, green: 0.224, blue: 0.208, alpha: 1)
-  }
+    
+    static var textGrey: UIColor {
+        return UIColor(red: 0.522, green: 0.514, blue: 0.569, alpha: 1)
+    }
+    
+    static var textDarkPurple: UIColor {
+        return UIColor(red: 0.259, green: 0.247, blue: 0.318, alpha: 1)
+    }
+    
+    static var blueMiniPlayer: UIColor {
+        return UIColor(red: 0.8, green: 0.882, blue: 0.984, alpha: 1)
+    }
+    
+    static var bluePlayer: UIColor {
+        return UIColor(red: 0.157, green: 0.51, blue: 0.945, alpha: 1)
+    }
+    
+    static var blueBorder: UIColor {
+        return UIColor(red: 0.157, green: 0.51, blue: 0.945, alpha: 1)
+    }
+    
+    static var deleteColor: UIColor {
+        return UIColor(red: 0.898, green: 0.224, blue: 0.208, alpha: 1)
+    }
+    
+    static var pinkBackground: UIColor {
+        return UIColor(red: 0.996, green: 0.851, blue: 0.839, alpha: 1)
+    }
+    
+    static var grayBackground: UIColor {
+        return UIColor(red: 0.93, green: 0.94, blue: 0.99, alpha: 1)
+    }
+    
+    static var symbolsLightPurple: UIColor {
+        return UIColor(red:0.52, green:0.59, blue:0.53, alpha:1)
+    }
 }
---
+++ PodcastApp/Screens/Profile/Profile/Views/ProfileSettingsViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  ProfileSettingsViewCell.swift
+//  PodcastApp
+//
+//  Created by Caroline Tikhomirova on 27.09.2023.
+//
+
+import UIKit
+
+class ProfileSettingsViewCell: UITableViewCell {
+    
+    static let identifier = "ProfileSettingsViewCell"
+    
+    private let iconBackground: UIView = {
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 48, height: 48)
+        view.layer.backgroundColor = UIColor.grayBackground.cgColor
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let icon = UIImageView()
+        icon.tintColor = .symbolsLightPurple
+        icon.contentMode = .scaleAspectFill
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.image = UIImage(systemName: "questionmark")
+        return icon
+    }()
+    
+    private let titleLabel = UILabel.makeLabelForCells(text: "line", font: .manropeRegular(size: 16), textColor: .textDarkPurple, numberOfLines: 0)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            self.selectionStyle = .none
+            setupView()
+        }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        contentView.addSubview(iconBackground)
+        contentView.addSubview(iconImageView)
+        contentView.addSubview(titleLabel)
+        backgroundColor = .clear
+        
+        NSLayoutConstraint.activate([
+            iconBackground.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            iconBackground.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            iconBackground.heightAnchor.constraint(equalToConstant: 48),
+            iconBackground.widthAnchor.constraint(equalToConstant: 48),
+            
+            iconImageView.centerYAnchor.constraint(equalTo: iconBackground.centerYAnchor),
+            iconImageView.centerXAnchor.constraint(equalTo: iconBackground.centerXAnchor),
+            iconImageView.heightAnchor.constraint(equalToConstant: 24),
+            iconImageView.widthAnchor.constraint(equalToConstant: 24),
+       
+            titleLabel.centerYAnchor.constraint(equalTo: iconBackground.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: iconBackground.trailingAnchor, constant: 16),
+            
+            
+        ])
+        
+    }
+    
+    func configure(image: UIImage, title: String) {
+        iconImageView.image = image
+        titleLabel.text = title
+        
+    }
+
+}
