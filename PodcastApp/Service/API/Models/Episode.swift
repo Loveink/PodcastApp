eca47f3271f5268342b83752af544ddf2155c347
Loveink <106765014+Loveink@users.noreply.github.com>
2023-09-30 13:23:38 +0300

    change episode model

--- PodcastApp/Service/API/Models/Episode.swift
+++ PodcastApp/Service/API/Models/Episode.swift
@@ -7,45 +7,20 @@
 
 import Foundation
 
-struct Episode: Decodable {
-    let status: Bool
-    let liveItems: [LiveEpisodeItem]?
-    let items: [EpisodeItem]
+struct EpisodeRandom: Decodable {
+    let status: String
+    let episodes: [EpisodeItem]
     let count: Int
-    let query: String?
+    let max: Int
     let description: String
 }
 
-struct LiveEpisodeItem: Decodable {
-    let id: Int
-    let title: String
-    let link: String
-    let description: String
-    let guid: String
-    let datePublished: TimeInterval
-    let datePublishedPretty: String
-    let dateCrawled: TimeInterval
-    let enclosureUrl: String
-    let enclosureType: String
-    let enclosureLength: Int
-    let startTime: TimeInterval
-    let endTime: TimeInterval
+struct EpisodeFeed: Decodable {
     let status: String
-    let contentLink: String
-    let duration: Int?
-    let explicit: Int
-    let episode: String?
-    let episodeType: String?
-    let season: String?
-    let image: String
-    let feedItunesId: Int
-    let feedImage: String
-    let feedId: Int
-    let feedLanguage: String
-    let feedDead: Int
-    let feedDuplicateOf: String?
-    let chaptersUrl: String?
-    let transcriptUrl: String?
+    let items: [EpisodeItem]
+    let count: Int
+    let query: String
+    let description: String
 }
 
 struct EpisodeItem: Decodable {
@@ -53,62 +28,8 @@ struct EpisodeItem: Decodable {
     let title: String
     let link: String
     let description: String
-    let guid: String
-    let datePublished: TimeInterval
-    let datePublishedPretty: String
-    let dateCrawled: TimeInterval
     let enclosureUrl: String
-    let enclosureType: String
     let enclosureLength: Int
-    let duration: Int?
-    let explicit: Int
-    let episode: String?
-    let episodeType: String?
-    let season: String?
     let image: String
-    let feedItunesId: Int
     let feedImage: String
-    let feedId: Int
-    let feedLanguage: String
-    let feedDead: Int
-    let feedDuplicateOf: String?
-    let chaptersUrl: String?
-    let transcriptUrl: String?
-    let persons: [Person]
-    let transcripts: [Transcript]
-    let value: Value
-}
-
-struct Person: Decodable {
-    let id: Int
-    let name: String
-    let role: String
-    let group: String
-    let href: String
-    let img: String
-}
-
-struct Transcript: Decodable {
-    let url: String
-    let type: String
-}
-
-struct Value: Decodable {
-    let model: Model
-    let destinations: [Destination]
-}
-
-struct Model: Decodable {
-    let type: String
-    let method: String
-    let suggested: String
-}
-
-struct Destination: Decodable {
-    let name: String
-    let type: String
-    let address: String
-    let split: Int
-    let customKey: String?
-    let customValue: String?
 }
