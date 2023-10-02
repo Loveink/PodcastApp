8a284875941b7a2856939f1d0ec938ab7e20079f
MAKSIM89PW <123>
2023-10-02 17:49:12 +0300

    поправилскролл коллекции

--- PodcastApp/Screens/Homepage/Controllers/HomePageViewController3.swift
+++ PodcastApp/Screens/Homepage/Controllers/HomePageViewController3.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class collectionViews {
 
-    static func collectionViewOne() -> UICollectionView {
-        
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        let collectionViewOne = UICollectionView(frame: CGRect(x: 32, y: 350, width: 311, height: 300), collectionViewLayout: layout)
-        return collectionViewOne
-        
-    }
+    
     
     static func collectionViewTwo() -> UICollectionView {
         
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        let collectionViewTwo = UICollectionView(frame: CGRect(x: 32, y: 100, width: 250, height: 250), collectionViewLayout: layout)
+        
+        let collectionViewTwo = UICollectionView(frame: CGRect(x: 32, y: 0, width: 250, height: 250), collectionViewLayout: layout)
+        collectionViewTwo.showsHorizontalScrollIndicator = false
         return collectionViewTwo
+        
+    }
+    
+    static func collectionViewOne() -> UICollectionView {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let collectionViewOne = UICollectionView(frame: CGRect(x: 32, y: 250, width: 311, height: 216), collectionViewLayout: layout)
+        collectionViewOne.showsHorizontalScrollIndicator = false
+        return collectionViewOne
         
     }
     
@@ -41,18 +47,18 @@ class HomePageViewController3: UIViewController {
     
     let cellReuseIdentifier = "MyCollectionViewCell3"
     
-    let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .white
-        return collectionView
-    }()
-    
+//    let collectionView: UICollectionView = {
+//        let layout = UICollectionViewFlowLayout()
+//        layout.scrollDirection = .horizontal
+//        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//        collectionView.backgroundColor = .white
+//        return collectionView
+//    }()
+//
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = R.Colors.background
         view.backgroundColor = R.Colors.background
+
         
         collectionViewOne.delegate = self
         collectionViewOne.dataSource = self
@@ -65,7 +71,6 @@ class HomePageViewController3: UIViewController {
         collectionViewTwo.register(CollectionViewCell2.self, forCellWithReuseIdentifier: cellReuseIdentifier)
         view.addSubview(collectionViewTwo)
         
-//        configureAppearance()
     }
 }
 
@@ -89,7 +94,7 @@ extension HomePageViewController3: UICollectionViewDataSource {
         if collectionView == self.collectionViewOne {
                let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath as IndexPath)
 
-//               myCell.backgroundColor = UIColor.red
+//               myCell.backgroundColor = R.Colors.background
 
                return myCell
 
@@ -112,8 +117,11 @@ extension HomePageViewController3: UICollectionViewDataSource {
 extension HomePageViewController3:  UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        // Установите размеры ячейки
-        return CGSize(width: 311, height: 72)
+        if collectionView == self.collectionViewOne {
+                    return CGSize(width: 311, height: 72)
+                } else {
+                    return CGSize(width: 250, height: 250)
+                }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
@@ -122,40 +130,40 @@ extension HomePageViewController3:  UICollectionViewDelegateFlowLayout {
     }
 }
 
-@objc extension HomePageViewController3 {
+//@objc extension HomePageViewController3 {
 
-    func customMyCell() -> UICollectionViewCell {
-        let indexPath = IndexPath()
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as! CollectionViewCell3
-        cell.backgroundColor = R.Colors.cellBackground
-        cell.layer.cornerRadius = 16
-        cell.layer.shadowColor = UIColor.black.cgColor
-        cell.layer.shadowOffset = CGSize(width: 0, height: 2) // Смещение тени (по горизонтали и вертикали)
-        cell.layer.shadowRadius = 4 // Радиус тени (чем больше, тем более размытой будет тень)
-        cell.layer.shadowOpacity = 0.2 // Прозрачность тени (0 - полностью прозрачная, 1 - полностью непрозрачная)
-        cell.layer.masksToBounds = false // Это позволяет тени выходить за границы ячейки
-        return cell
-    }
+//    func customMyCell() -> UICollectionViewCell {
+//        let indexPath = IndexPath()
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as! CollectionViewCell3
+//        cell.backgroundColor = R.Colors.cellBackground
+//        cell.layer.cornerRadius = 16
+//        cell.layer.shadowColor = UIColor.black.cgColor
+//        cell.layer.shadowOffset = CGSize(width: 0, height: 2) // Смещение тени (по горизонтали и вертикали)
+//        cell.layer.shadowRadius = 4 // Радиус тени (чем больше, тем более размытой будет тень)
+//        cell.layer.shadowOpacity = 0.2 // Прозрачность тени (0 - полностью прозрачная, 1 - полностью непрозрачная)
+//        cell.layer.masksToBounds = false // Это позволяет тени выходить за границы ячейки
+//        return cell
+//    }
 
-    func configureAppearance() {
-        collectionView.backgroundColor = R.Colors.background
-
-        collectionView.register(CollectionViewCell3.self, forCellWithReuseIdentifier: cellReuseIdentifier)
-
-        collectionView.dataSource = self
-        collectionView.delegate = self
-
-        view.addSubview(collectionView)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ])
-
-        view.backgroundColor = R.Colors.background
-    }
-}
+//    func configureAppearance() {
+//        collectionView.backgroundColor = R.Colors.background
+//
+//        collectionView.register(CollectionViewCell3.self, forCellWithReuseIdentifier: cellReuseIdentifier)
+//
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+//
+//        view.addSubview(collectionView)
+//        collectionView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        NSLayoutConstraint.activate([
+//            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+//            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//        ])
+//
+//        view.backgroundColor = R.Colors.background
+//    }
+//}
 
