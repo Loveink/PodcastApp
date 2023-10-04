57672cc10968a42eea29028d39ecc2e29e3b0031
michaelbolgar <mr.mbolgar@inbox.ru>
2023-10-04 02:03:05 +0200

    configured firebase & google login

--- PodcastApp/Screens/Authorization/Login/Controllers/LoginViewController.swift
+++ PodcastApp/Screens/Authorization/Login/Controllers/LoginViewController.swift
@@ -1,10 +1,28 @@
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
 
-    //MARK: - Properties
+    // MARK: - Properties
 
     let loginView = LoginView()
+
+    // MARK: - UI Elements
+
+    private lazy var googleButton: GIDSignInButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        //        $0.setTitle("Continue with Google", for: .normal)
+        //        $0.setTitleColor(.black, for: .normal)
+        //        $0.titleLabel?.font = UIFont.manropeBold(size: 16)
+        $0.backgroundColor = UIColor.white
+        $0.layer.cornerRadius = 28
+        $0.layer.borderColor = UIColor.black.cgColor
+        $0.layer.borderWidth = 1
+        //        $0.style = .iconOnly
+        $0.addTarget(self, action: #selector(googleButtonAction), for: .touchUpInside)
+        return $0
+    }(GIDSignInButton())
 
     // MARK: - Init
 
@@ -18,13 +36,46 @@ class LoginViewController: UIViewController {
     private func layout() {
 
         view.addSubview(loginView)
+        view.addSubview(googleButton)
 
         NSLayoutConstraint.activate([
 
             loginView.topAnchor.constraint(equalTo: view.topAnchor),
             loginView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             loginView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            loginView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            loginView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            googleButton.bottomAnchor.constraint(equalTo: loginView.bottomAnchor, constant: -250),
+            googleButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            googleButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            googleButton.heightAnchor.constraint(equalToConstant: 57)
         ])
     }
+
+    @objc private func googleButtonAction() {
+        print ("google button tapped")
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            if let error = error {
+                print ("Error by authorization: \(error.localizedDescription)")
+                return
+            }
+
+            guard let user = result?.user,
+                  let idToken = user.idToken?.tokenString
+            else {
+                return
+            }
+
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: user.accessToken.tokenString)
+            print (user.userID as Any, user.idToken as Any)
+        }
+    }
 }
+
+
--- PodcastApp/Screens/Authorization/Login/Views/LoginView.swift
+++ PodcastApp/Screens/Authorization/Login/Views/LoginView.swift
@@ -1,5 +1,7 @@
 import Foundation
 import UIKit
+import Firebase
+import FirebaseAuth
 
 
 //добавить распознаватель тапа на лейбл внизу
@@ -101,21 +103,44 @@ class LoginView: UIView {
             continueLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             continueLabel.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: updownInset*4),
 
+            //            googleButton.topAnchor.constraint(equalTo: continueLabel.bottomAnchor, constant: updownInset*2),
+            //            googleButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: sideInset),
+            //            googleButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -sideInset),
+            //            googleButton.heightAnchor.constraint(equalToConstant: 57),
+
             signupLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             signupLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -100)
         ])
     }
 
     @objc private func loginButtonAction() {
-        var responder: UIResponder? = loginButton
-        while let nextResponder = responder?.next {
-            if let viewController = nextResponder as? UIViewController {
-                let createAccountVC = CreateAccountViewController()
-                createAccountVC.modalPresentationStyle = .fullScreen
-                viewController.present(createAccountVC, animated: true, completion: nil)
-                break
+
+        if let email = loginField.text, let password = passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                if let error = error {
+                    print("Ошибка входа: \(error.localizedDescription)")
+                } else {
+                    if let user = user {
+                        print("Пользователь авторизован: \(user)")
+                        var responder: UIResponder? = self.loginButton
+                        while let nextResponder = responder?.next {
+                            if let viewController = nextResponder as? UIViewController {
+                                let createAccountVC = CreateAccountViewController()
+                                createAccountVC.modalPresentationStyle = .fullScreen
+                                viewController.present(createAccountVC, animated: true, completion: nil)
+                                break
+                            }
+                            responder = nextResponder
+                        }
+                    }
+                }
             }
-            responder = nextResponder
+        }
+
+        if let user = Auth.auth().currentUser {
+            let uid = user.uid
+            let email = user.email
+            print (uid, email ?? "no email")
         }
     }
 }
