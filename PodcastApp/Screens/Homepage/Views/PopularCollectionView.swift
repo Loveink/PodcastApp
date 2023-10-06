01758d01e3b4f040d9d9b44631e48ed96bb8abf2
Anastasia <a_ribakova@mail.ru>
2023-10-06 13:30:46 +0300

    add topView on HomeVC
    add topView on HomeVC

--- PodcastApp/Screens/Homepage/Views/PopularCollectionView.swift
+++ PodcastApp/Screens/Homepage/Views/PopularCollectionView.swift
@@ -9,85 +9,87 @@ import UIKit
 
 class PopularCollectionView: UIView {
 
-  var collectionView: UICollectionView!
-  weak var delegate: PopularCollectionViewDelegate?
-
-  var podcasts: [PodcastItemCell] = [] {
-    didSet {
-      DispatchQueue.main.async {
-        self.collectionView.reloadData()
-      }
+    var collectionView: UICollectionView!
+    weak var delegate: PopularCollectionViewDelegate?
+    
+    var podcasts: [PodcastItemCell] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
     }
-  }
-
-  var selectedCell: PodcastCell?
-  var id: Int?
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    configureCollection()
-    addSubview(collectionView)
-    setupConstraints()
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  func configureCollection() {
-    let layout = UICollectionViewFlowLayout()
-    layout.scrollDirection = .vertical
-    collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
-    collectionView.showsHorizontalScrollIndicator = false
-    collectionView.backgroundColor = .clear
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    collectionView.register(PodcastCell.self, forCellWithReuseIdentifier: PodcastCell.identifier)
-    collectionView.delegate = self
-    collectionView.dataSource = self
-  }
-
-  private func setupConstraints() {
-    NSLayoutConstraint.activate([
-      collectionView.topAnchor.constraint(equalTo: topAnchor),
-      collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
-      collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
-      collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
-    ])
-  }
+    
+    var selectedCell: PodcastCell?
+    var id: Int?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureCollection()
+        addSubview(collectionView)
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCollection() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(PodcastCell.self, forCellWithReuseIdentifier: PodcastCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+    
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
 
 //MARK: - Extensions
 extension PopularCollectionView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return podcasts.count
-  }
-
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PodcastCell.identifier, for: indexPath) as? PodcastCell else {
-      return UICollectionViewCell()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return podcasts.count
     }
-    cell.configureCell(podcasts[indexPath.row])
-    cell.id = Int(podcasts[indexPath.row].id)
-    return cell
-  }
-
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-      let cellWidth = collectionView.frame.width - 20
-      return CGSize(width: cellWidth, height: 80)
-  }
-
-  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    if podcasts.count > 0 {
-      if let selectedCell = collectionView.cellForItem(at: indexPath) as? PodcastCell,
-         let id = selectedCell.id {
-        delegate?.didSelectPodcast(id: id)
-      }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PodcastCell.identifier, for: indexPath) as? PodcastCell else {
+            return UICollectionViewCell()
+        }
+        cell.configureCell(podcasts[indexPath.row])
+        cell.id = Int(podcasts[indexPath.row].id)
+        return cell
     }
-  }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = collectionView.frame.width - 20
+        return CGSize(width: cellWidth, height: 80)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if podcasts.count > 0 {
+            if let selectedCell = collectionView.cellForItem(at: indexPath) as? PodcastCell,
+               let id = selectedCell.id {
+                delegate?.didSelectPodcast(id: id)
+            }
+        }
+    }
 }
 
 //MARK: - Protocols
 protocol PopularCollectionViewDelegate: AnyObject {
-  func didSelectPodcast(id: Int)
+    func didSelectPodcast(id: Int)
 }
--- PodcastApp/Screens/Homepage/Views/TrendingCollectionView.swift
+++ PodcastApp/Screens/Homepage/Views/TrendingCollectionView.swift
@@ -8,85 +8,107 @@
 import UIKit
 
 class TrendingCollectionView: UIView {
-  
-  var collectionView: UICollectionView!
-  weak var delegate: TrendingCollectionViewDelegate?
-  
-  var podcasts: [CategoryInfoForCell] = []
-  var categoryDictionary: [String: String] = [:] {
-      didSet {
-        for (categoryName, _) in categoryDictionary {
-          let categoryInfo = CategoryInfoForCell(categoryName: categoryName, episodeCount: Int.random(in: 70...100))
-          podcasts.append(categoryInfo)
+    
+    private lazy var categoryLabel = UILabel.makeLabel(
+        text: "Category",
+        font: UIFont.plusJakartaSansBold(size: 16),
+        textColor: UIColor.black
+    )
+    
+    private lazy var seeAllLabel = UILabel.makeLabel(
+        text: "See all",
+        font: UIFont.plusJakartaSansMedium(size: 16),
+        textColor: UIColor.gray
+    )
+    
+    var collectionView: UICollectionView!
+    weak var delegate: TrendingCollectionViewDelegate?
+    
+    var podcasts: [CategoryInfoForCell] = []
+    var categoryDictionary: [String: String] = [:] {
+        didSet {
+            for (categoryName, _) in categoryDictionary {
+                let categoryInfo = CategoryInfoForCell(categoryName: categoryName, episodeCount: Int.random(in: 70...100))
+                podcasts.append(categoryInfo)
+            }
+            collectionView.reloadData()
         }
-        collectionView.reloadData()
-      }
     }
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    configureCollection()
-    setupConstraints()
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  func configureCollection() {
-    let layout = UICollectionViewFlowLayout()
-    layout.scrollDirection = .horizontal
-    collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
-    collectionView.showsHorizontalScrollIndicator = false
-    collectionView.backgroundColor = .clear
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
-    collectionView.register(CategoriesCell.self, forCellWithReuseIdentifier: CategoriesCell.identifier)
-    collectionView.delegate = self
-    collectionView.dataSource = self
-  }
-  
-  private func setupConstraints() {
-    self.addSubview(collectionView)
-    NSLayoutConstraint.activate([
-      collectionView.topAnchor.constraint(equalTo: topAnchor),
-      collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
-      collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
-      collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
-    ])
-  }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureCollection()
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCollection() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(CategoriesCell.self, forCellWithReuseIdentifier: CategoriesCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    private func setupConstraints() {
+        addSubview(categoryLabel)
+        addSubview(seeAllLabel)
+        self.addSubview(collectionView)
+    
+        NSLayoutConstraint.activate([
+            
+            categoryLabel.topAnchor.constraint(equalTo: topAnchor),
+            categoryLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            
+            seeAllLabel.topAnchor.constraint(equalTo: topAnchor),
+            seeAllLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 0),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
 
 //MARK: - Extensions
 extension TrendingCollectionView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-  
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return podcasts.count
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoriesCell.identifier, for: indexPath) as? CategoriesCell else {
-          return UICollectionViewCell()
-      }
-
-      let category = podcasts[indexPath.row]
-      cell.configureCell(categoryName: category.categoryName, episodeCount: category.episodeCount)
-      return cell
-  }
-
-  
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    return CGSize(width: 144, height: 200)
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    if podcasts.count > 0 {
-      let selectedPodcast = podcasts[indexPath.item]
-      delegate?.didSelectPodcastName(selectedPodcast)
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return podcasts.count
     }
-  }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoriesCell.identifier, for: indexPath) as? CategoriesCell else {
+            return UICollectionViewCell()
+        }
+        
+        let category = podcasts[indexPath.row]
+        cell.configureCell(categoryName: category.categoryName, episodeCount: category.episodeCount)
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 144, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if podcasts.count > 0 {
+            let selectedPodcast = podcasts[indexPath.item]
+            delegate?.didSelectPodcastName(selectedPodcast)
+        }
+    }
 }
 
 //MARK: - Protocols
 protocol TrendingCollectionViewDelegate: AnyObject {
-  func didSelectPodcastName(_ selectedPodcast: CategoryInfoForCell)
+    func didSelectPodcastName(_ selectedPodcast: CategoryInfoForCell)
 }
